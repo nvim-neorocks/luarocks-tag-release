3174618e30814e4b8507467bc74eabb9dd3da0f6
Matthieu Coudron <mcoudron@hotmail.com>
2023-03-13 23:51:56 +0100

    test2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,13 @@
 
 nix build "/pkg#luarocks-tag-release"
 
+echo "GITHUB_EVENT_NAME: $1"
+IS_PR=0
+if [ IS_PR =~ "pull_request" ]; then
+  echo "THIS IS a PULL_REQUEST"
+else
+  echo "not a PULL_REQUEST"
+fi
+
+shift
 ./result/bin/luarocks-tag-release "$@"
