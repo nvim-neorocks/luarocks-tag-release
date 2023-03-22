edaa93d52e6440ceab66030fff9096a91f755b15
Marc Jakobi <mrcjkb89@outlook.com>
2023-03-22 22:45:26 +0100

    ci: replace linter action with nix and reorganise flake (#42)

--- flake.nix
+++ flake.nix
@@ -8,22 +8,25 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     pre-commit-hooks,
     ...
   }: let
     supportedSystems = [
       "x86_64-linux"
     ];
-    perSystem = nixpkgs.lib.genAttrs supportedSystems;
-    pkgsFor = system: import nixpkgs {inherit system;};
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+      inherit (pkgs) lib;
 
-    luarocks-tag-release-action-for = system: let
-      pkgs = pkgsFor system;
       luarocks-tag-release-wrapped = pkgs.lua51Packages.buildLuaApplication {
         pname = "luarocks-tag-release";
         version = "scm-1";
@@ -35,8 +38,8 @@
           luafilesystem
         ];
       };
-    in
-      pkgs.writeShellApplication {
+
+      luarocks-tag-release-action = pkgs.writeShellApplication {
         name = "luarocks-tag-release-action";
         runtimeInputs = with pkgs; [
           curl
@@ -54,12 +57,34 @@
         # The default checkPhase depends on ShellCheck, which depends on GHC
         checkPhase = "";
       };
-  in {
-    packages = perSystem (system: let
-      luarocks-tag-release-action = luarocks-tag-release-action-for system;
+
+      formatting = pre-commit-hooks.lib.${system}.run {
+        src = self;
+        hooks = {
+          alejandra.enable = true;
+          stylua.enable = true;
+          luacheck.enable = true;
+          editorconfig-checker.enable = true;
+          markdownlint.enable = true;
+        };
+        settings = {
+          markdownlint.config = {
+            MD004 = false;
+            MD012 = false;
+            MD013 = false;
+            MD022 = false;
+            MD031 = false;
+            MD032 = false;
+          };
+        };
+      };
     in {
-      default = luarocks-tag-release-action;
-      inherit luarocks-tag-release-action;
+      packages = {
+        default = luarocks-tag-release-action;
+        inherit luarocks-tag-release-action;
+      };
+      checks = {
+        inherit formatting;
+      };
     });
-  };
 }
