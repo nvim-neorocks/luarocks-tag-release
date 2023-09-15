806c92393402ca3a18df82187cc7227049c7be1d
Marc Jakobi <marc@jakobi.dev>
2023-09-15 16:16:22 +0200

    refactor: extract pure logic into separate modules (#113)

--- bin/luarocks-tag-release-action.lua
+++ bin/luarocks-tag-release-action.lua
@@ -1,5 +1,8 @@
 assert(os.getenv('LUAROCKS_API_KEY'), 'LUAROCKS_API_KEY secret not set')
 
+local Parser = require('ltr.parser')
+local OS = require('ltr.os')
+
 local function getenv_or_err(env_var)
   return assert(os.getenv(env_var), env_var .. ' not set.')
 end
@@ -22,80 +25,6 @@ local repo_name = assert(
 
 local github_server_url = getenv_or_err('GITHUB_SERVER_URL')
 
----@param str string
----@return string[] list_arg
-local function parse_list_args(str)
-  local tbl = {}
-  for arg in string.gmatch(str, '[^\r\n]+') do
-    table.insert(tbl, arg)
-  end
-  return tbl
-end
-
----Filter out directories that don't exist.
----@param directories string[] List of directories.
----@return string[] existing_directories
-local function filter_existing_directories(directories)
-  local existing_directories = {}
-  for _, dir in pairs(directories) do
-    if require('lfs').attributes(dir, 'mode') == 'directory' then
-      existing_directories[#existing_directories + 1] = dir
-    end
-  end
-  return existing_directories
-end
-
----Insert Neovim plugin directories into the `copy_directories` list
----@param copy_directories string[] List of directories
-local function insert_neovim_plugin_dirs(copy_directories)
-  local neovim_plugin_dirs = {
-    'autoload',
-    'colors',
-    'compiler',
-    'doc',
-    'filetype.lua',
-    'ftplugin',
-    'indent',
-    'keymap',
-    'lang',
-    'menu.vim',
-    'parser',
-    'plugin',
-    'queries',
-    'query',
-    'rplugin',
-    'spell',
-    'syntax',
-  }
-  for _, dir in pairs(neovim_plugin_dirs) do
-    table.insert(copy_directories, dir)
-  end
-end
-
----@param str_args string The arguments to parse
----@return string[] copy_directories The directories to copy
-local function parse_copy_directory_args(str_args)
-  local args = parse_list_args(str_args)
-  local copy_directories = {}
-  for _, arg in pairs(args) do
-    if string.match(arg, '{{ neovim%.plugin%.dirs }}') then
-      insert_neovim_plugin_dirs(copy_directories)
-    else
-      copy_directories[#copy_directories + 1] = arg
-    end
-  end
-  return filter_existing_directories(copy_directories)
-end
-
-local function list_has_lua(t)
-  for _, v in pairs(t) do
-    if v == 'lua' or (type(v) == 'string' and string.match(v, '^lua%s+')) then
-      return true
-    end
-  end
-  return false
-end
-
 local is_pull_request = getenv_or_empty('GITHUB_EVENT_NAME') == 'pull_request'
 
 local license_input = os.getenv('INPUT_LICENSE')
@@ -104,32 +33,20 @@ local package_name = getenv_or_err('INPUT_NAME')
 local package_version = is_pull_request and '0.0.0' or getenv_or_err('INPUT_VERSION')
 
 local interpreters_input = os.getenv('INPUT_TEST_INTERPRETERS')
----@type lua_interpreter[]
-local test_interpreters = {}
-if interpreters_input then
-  for _, input in pairs(parse_list_args(interpreters_input)) do
-    if input == 'neovim-stable' then
-      table.insert(test_interpreters, 'neolua')
-    elseif input == 'neovim-nightly' then
-      table.insert(test_interpreters, 'neolua-nightly')
-    elseif input == 'lua' then
-      table.insert(test_interpreters, 'lua')
-    else
-      error('Test interpreter ' .. input .. ' not supported.')
-    end
-  end
-end
+local test_interpreters = Parser.parse_interpreter_input(interpreters_input)
 
 ---@type Args
 local args = {
   github_repo = github_repo,
   repo_name = repo_name,
   github_server_url = github_server_url,
-  dependencies = parse_list_args(getenv_or_empty('INPUT_DEPENDENCIES')),
-  labels = parse_list_args(getenv_or_empty('INPUT_LABELS')),
-  copy_directories = parse_copy_directory_args(getenv_or_err('INPUT_COPY_DIRECTORIES')),
+  dependencies = Parser.parse_list_args(getenv_or_empty('INPUT_DEPENDENCIES')),
+  labels = Parser.parse_list_args(getenv_or_empty('INPUT_LABELS')),
+  copy_directories = OS.filter_existing_directories(
+    Parser.parse_copy_directory_args(getenv_or_err('INPUT_COPY_DIRECTORIES'))
+  ),
   summary = getenv_or_empty('INPUT_SUMMARY'),
-  detailed_description_lines = parse_list_args(getenv_or_empty('INPUT_DETAILED_DESCRIPTION')),
+  detailed_description_lines = Parser.parse_list_args(getenv_or_empty('INPUT_DETAILED_DESCRIPTION')),
   rockspec_template_file_path = template_input ~= '' and template_input
     or action_path .. '/resources/rockspec.template',
   upload = not is_pull_request,
@@ -139,9 +56,6 @@ local args = {
   ref_type = getenv_or_err('GITHUB_REF_TYPE'),
   git_ref = getenv_or_err('GITHUB_REF_NAME'),
 }
-if not list_has_lua(args.dependencies) then
-  table.insert(args.dependencies, 1, 'lua >= 5.1')
-end
 
 print('Workflow has been triggered by: ' .. args.ref_type)
 local is_tag = args.ref_type == 'tag'
---
+++ lua/ltr/rockspec.lua
@@ -0,0 +1,120 @@
+local Rockspec = {}
+
+---@param t string[]
+---@return boolean
+local function list_has_lua(t)
+  for _, v in pairs(t) do
+    if v == 'lua' or (type(v) == 'string' and string.match(v, '^lua%s+')) then
+      return true
+    end
+  end
+  return false
+end
+
+---@class (exact) GenerateMeta
+---@field ref_type github_ref_type
+---@field github_server_url string The github server's URL.
+---@field github_repo string The github repository (owner/repo_name).
+---@field license string|nil License SPDX ID (optional).
+---@field git_ref string E.g. a tag or a commit sha.
+---@field summary string Package summary.
+---@field detailed_description_lines string[] Detailed description (list of lines).
+---@field dependencies string[] List of LuaRocks package dependencies.
+---@field labels string[] List of labels to add to the rockspec.
+---@field copy_directories string[] List of directories to add to the rockspec's copy_directories.
+---@field repo_name string The repository name.
+---@field github_event_tbl any|nil GitHub event metadata, read from GITHUB_EVENT_PATH and decoded from JSON
+
+---Generate a rockspec from a template
+---@param package_name string The name of the LuaRocks package.
+---@param modrev string the version of the package - in format 3.0 must follow the format '[%w.]+-[%d]+'
+---@param specrev string the version of the rockspec
+---@param rockspec_template string The template rockspec
+---@param meta GenerateMeta
+---@return string
+function Rockspec.generate(package_name, modrev, specrev, rockspec_template, meta)
+  local archive_dir_suffix = meta.ref_type == 'tag' and modrev or meta.git_ref
+
+  ---@param xs string[]?
+  ---@return string lua_list_string
+  local function mk_lua_list_string(xs)
+    if not xs or #xs == 0 then
+      return '{ }'
+    end
+    return "{ '" .. table.concat(xs, "', '") .. "' } "
+  end
+
+  ---@param xs string[]?
+  ---@return string lua_multiline_string
+  local function mk_lua_multiline_str(xs)
+    if not xs or #xs == 0 then
+      return "''"
+    end
+    return '[[\n    ' .. table.concat(xs, '\n    ') .. '  \n]]'
+  end
+
+  local repo_url = meta.github_server_url .. '/' .. meta.github_repo
+  local homepage = repo_url
+  local license
+  local repo_meta = meta.github_event_tbl
+    and (
+      meta.github_event_tbl.pull_request
+        and meta.github_event_tbl.pull_request.head
+        and meta.github_event_tbl.pull_request.head.repo
+      or meta.github_event_tbl.repository
+    )
+  if repo_meta then
+    local repo_license = repo_meta.license or repo_meta.source and repo_meta.source.license
+    if meta.license then
+      license = "license = '" .. meta.license .. "'"
+    elseif repo_license and repo_license.spdx_id ~= '' and repo_license.spdx_id ~= 'NOASSERTION' then
+      license = "license = '" .. repo_license.spdx_id .. "'"
+    else
+      error([[
+    Could not get the license SPDX ID from the GitHub API.
+    Please add a license file that GitHub can recognise or add a `license` input.
+    See: https://github.com/nvim-neorocks/luarocks-tag-release#license
+    ]])
+    end
+    if not meta.summary or meta.summary == '' then
+      meta.summary = repo_meta.description and repo_meta.description or ''
+    end
+    if #meta.labels == 0 then
+      meta.labels = repo_meta.topics and repo_meta.topics or {}
+    end
+    if repo_meta.homepage and repo_meta.homepage ~= '' then
+      homepage = repo_meta.homepage
+    end
+  end
+
+  ---@param str string
+  ---@return string
+  local function escape_quotes(str)
+    local escaped = str:gsub("'", "\\'")
+    return escaped
+  end
+
+  if not list_has_lua(meta.dependencies) then
+    table.insert(meta.dependencies, 1, 'lua >= 5.1')
+  end
+
+  local rockspec = rockspec_template
+    :gsub('$git_ref', meta.git_ref)
+    :gsub('$modrev', modrev)
+    :gsub('$specrev', specrev)
+    :gsub('$repo_url', repo_url)
+    :gsub('$archive_dir_suffix', archive_dir_suffix)
+    :gsub('$package', package_name)
+    :gsub('$summary', escape_quotes(meta.summary))
+    :gsub('$detailed_description', mk_lua_multiline_str(meta.detailed_description_lines))
+    :gsub('$dependencies', mk_lua_list_string(meta.dependencies))
+    :gsub('$labels', mk_lua_list_string(meta.labels))
+    :gsub('$homepage', homepage)
+    :gsub('$license', license)
+    :gsub('$copy_directories', mk_lua_list_string(meta.copy_directories))
+    :gsub('$repo_name', meta.repo_name)
+
+  return rockspec
+end
+
+return Rockspec
