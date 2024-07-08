d37e4c8b2f8293b6a6354b5844ae9a29390327a0
Matthieu C. <886074+teto@users.noreply.github.com>
2024-07-08 15:12:03 +0200

    wip

---
+++ bin/rockspec-generator-json.lua
@@ -0,0 +1,31 @@
+#!/usr/bin/env lua
+local json = require('dkjson')
+
+json.decode(github_event_data)
+
+local input = io.stdin:read("*all")
+print("You entered: " .. input)
+
+package_name = input.name
+dependencies = input.dependencies
+license = input.license
+
+local rockspec = require('ltr.rockspec').generate(
+  package_name, modrev, specrev, rockspec_template,
+    {
+    ref_type = args.ref_type,
+    git_server_url = args.git_server_url,
+    github_repo = args.github_repo,
+    license = args.license,
+    git_ref = args.git_ref,
+    summary = args.summary,
+    detailed_description_lines = args.detailed_description_lines,
+    dependencies = args.dependencies,
+    test_dependencies = args.test_dependencies,
+    labels = args.labels,
+    copy_directories = args.copy_directories,
+    repo_name = args.repo_name,
+    github_event_tbl = github_event_tbl,
+  })
+
+
